dafa732f055f6c1abedd5f71e5a954ec40484005
Nickolay Savchenko <n.savchenko@axioma.lv>
2018-11-30 20:16:30 +0300

    37779 bonus test

--- test/crowdsale.js
+++ test/crowdsale.js
@@ -1,14 +1,16 @@
 import expectThrow from './helpers/expectThrow';
 
-var Crowdsale = artifacts.require('./test/TestCrowdsale.sol');
-var Token = artifacts.require('./token-sale-contracts/TokenSale/Token/Token.sol');
-var Creator = artifacts.require('./token-sale-contracts/TokenSale/Crowdsale/Creator.sol');
+const Crowdsale = artifacts.require('./test/TestCrowdsale.sol');
+const Token = artifacts.require('./token-sale-contracts/TokenSale/Token/Token.sol');
+const Creator = artifacts.require('./token-sale-contracts/TokenSale/Crowdsale/Creator.sol');
+
+const web3 = Token.web3;
 
 contract('Crowdsale', function(accounts) {
 
   it('should have test accounts in wallets', async function() {
-    let crowdsale = await Crowdsale.deployed();
-    let creator = await Creator.deployed();
+    const crowdsale = await Crowdsale.deployed();
+    const creator = await Creator.deployed();
 
     assert.equal(await crowdsale.wallets(0), accounts[0], 'Invalid Beneficiary address');
     assert.equal(await crowdsale.wallets(1), accounts[1], 'Invalid Accountant address');
@@ -22,4 +24,86 @@ contract('Crowdsale', function(accounts) {
     assert.equal(await crowdsale.wallets(9), accounts[9], 'Invalid Fees address');
   });
 
+  it('should add time and volume bonuses', async function() {
+    const crowdsale = await Crowdsale.deployed();
+    const creator = await Creator.deployed();
+    const token = Token.at(await crowdsale.token());
+
+    const now = Math.floor((new Date()).getTime() / 1000);
+
+    if (!await crowdsale.isInitialized()) {
+      await crowdsale.setStartTime(now + 24 * 3600);
+      await crowdsale.initialize({from: accounts[2]});
+      await crowdsale.setStartTime(now);
+    }
+
+    let balance = 0;
+    assert.equal(0, parseInt(await token.balanceOf(accounts[10])), '0 tokens wasn\'t on accounts[10]');
+    assert.equal(0, parseInt(await token.freezedTokenOf(accounts[10])), '0 freezed tokens wasn\'t on accounts[10]');
+
+    // min pay: web3.toWei(71, 'finney')
+    // value bonus 30%: web3.toWei(71, 'ether')
+    // time bonus 15%: 1 day
+    // time bonus 10%: 2 days
+    // time bonus 5%: 4 days
+
+    const VBValue = web3.toWei(71, 'ether');
+    const VBBonus = 1.3;
+    const TB1day = 1.15;
+    const TB3day = 1.1;
+    const TB7day = 1.05;
+
+    const rate = await crowdsale.rate(); // 10000 ether
+    const amount = web3.toWei(1, 'ether');
+    const getTokens = (wei) => rate * wei / web3.toWei(1, 'ether');
+    const assertBNEqual = (actual, expected, message) => {
+      return assert.equal(
+        Math.round(actual / web3.toWei(1, 'ether')),
+        Math.round(expected / web3.toWei(1, 'ether')),
+        message
+      );
+    };
+
+    await crowdsale.setStartTime(now - 60);
+
+    // Time bonus 1 day
+    balance += getTokens(amount) * TB1day;
+    await crowdsale.buyTokens(accounts[10], {from: accounts[10], value: amount});
+
+    assertBNEqual(balance, await token.balanceOf(accounts[10]), 'invalid purchase with 1 day time bonus');
+
+    // Maximum of Time bonus 1 day and Volume bonus
+    balance += getTokens(VBValue) * VBBonus;
+    await crowdsale.buyTokens(accounts[10], {from: accounts[10], value: VBValue});
+
+    assertBNEqual(balance, await token.balanceOf(accounts[10]), 'invalid purchase with 1 day time bonus and volume bonus');
+
+    await crowdsale.setStartTime(now - 24 * 3600);
+
+    // Time bonus 3 day
+    balance += getTokens(amount) * TB3day;
+    await crowdsale.buyTokens(accounts[10], {from: accounts[10], value: amount});
+    assertBNEqual(balance, await token.balanceOf(accounts[10]), 'invalid purchase with 2 days time bonus');
+
+    await crowdsale.setStartTime(now - 3 * 24 * 3600);
+
+    // Time bonus 7 day
+    balance += getTokens(amount) * TB7day;
+    await crowdsale.buyTokens(accounts[10], {from: accounts[10], value: amount});
+    assertBNEqual(balance, await token.balanceOf(accounts[10]), 'invalid purchase with 4 days time bonus');
+
+    await crowdsale.setStartTime(now - 7 * 24 * 3600);
+
+    // No bonus
+    balance += getTokens(amount);
+    await crowdsale.buyTokens(accounts[10], {from: accounts[10], value: amount});
+    assertBNEqual(balance, await token.balanceOf(accounts[10]), 'invalid purchase without bonus');
+
+    // Volume bonus
+    balance += getTokens(VBValue) * VBBonus;
+    await crowdsale.buyTokens(accounts[10], {from: accounts[10], value: VBValue});
+    assertBNEqual(balance, await token.balanceOf(accounts[10]), 'invalid purchase with volume bonus');
+
+    assert.equal(0, parseInt(await token.freezedTokenOf(accounts[10])), '0 freezed tokens wasn\'t on accounts[10]');
+  });
 });
